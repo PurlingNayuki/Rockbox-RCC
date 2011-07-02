6d229ab19c02d050bc2778167404a3efa5508173
PurlingNayuki <cyq.yzfl@gmail.com>
2011-07-02 21:19:25 +0800

    *改进自动编译脚本
    去除了检测svn更新的函数；
    支持使用-j参数来启用多核心加速编译；
    编译时编译所有usable及stable型号；
    完善提示信息；

--- autobuild.pl
+++ autobuild.pl
@@ -9,26 +9,22 @@ if($ARGV[0] eq "-v") {
     shift @ARGV;
 }
 
-my $update;
-if($ARGV[0] eq "-u") {
-    $update =1;
+my $mutilcore;
+if($ARGV[0] eq "-j") {
+    $mutilcore =1;
     shift @ARGV;
 }
 
 my $doonly;
 if($ARGV[0]) {
     $doonly = $ARGV[0];
-    print "only build $doonly\n" if($verbose);
+    print "Will only build $doonly\n" if($verbose);
 }
 
-if($update) {
-    # svn update!
-    system("svn -q up");
-}
 
-$rev = `svnversion`;
+$rev = `git describe --tags --always`;
 chomp $rev;
-print "rev $rev\n" if($verbose);
+print "Rockbox Rev: $rev\n\n" if($verbose);
 
 # made once for all targets
 sub runone {
@@ -41,66 +37,25 @@ sub runone {
 
     mkdir "build-$dir";
     chdir "build-$dir";
-    print "Build in build-$dir\n" if($verbose);
+    print "\nBuilding in build-$dir" if($verbose);
 
-    # build the manual(s)
     $a = buildit($dir, $confnum, $extra);
 
     chdir "..";
 
-    my $o="build-$dir/rockbox.zip";
-    my $map="build-$dir/rockbox-maps.zip";
-    my $elf="build-$dir/rockbox-elfs.zip";
+    my $o="build-$dir/rockbox";
     if (-f $o) {
-        my $newo="output/rockbox-$dir-$version.zip";
-        my $newmap="output/rockbox-$dir-$version-maps.zip";
-        my $newelf="output/rockbox-$dir-$version-elfs.zip";
-        system("mkdir -p output");
-        system("mv $o $newo");
-        print "moved $o to $newo\n" if($verbose);
-        system("mv $map $newmap");
-        print "moved $map to $newmap\n" if($verbose);
-        system("mv $elf $newelf");
-        print "moved $elf to $newelf\n" if($verbose);
+        my $newo="output/rockbox-$dir-$version";
+        
+        `mkdir -p output`;
+        `mv $o.zip $newo.zip`;
     }
 
-    print "remove all contents in build-$dir\n" if($verbose);
+    print "Removing all contents in build-$dir\n" if($verbose);
     system("rm -rf build-$dir");
 
     return $a;
 };
-
-sub fonts {
-    my ($dir, $confnum, $newl)=@_;
-    my $a;
-
-    if($doonly && ($doonly ne $dir)) {
-        return;
-    }
-
-    mkdir "build-$dir";
-    chdir "build-$dir";
-    print "Build fonts in build-$dir\n" if($verbose);
-
-    # build the fonts
-    $a = buildfonts($dir, $confnum, $newl);
-
-    chdir "..";
-
-    my $o="build-$dir/rockbox-fonts.zip";
-    if (-f $o) {
-        my $newo="output/rockbox-fonts-$version.zip";
-        system("mv $o $newo");
-        print "moved $o to $newo\n" if($verbose);
-    }
-
-    print "remove all contents in build-$dir\n" if($verbose);
-    system("rm -rf build-$dir");
-
-    return $a;
-};
-
-
 
 sub buildit {
     my ($target, $confnum, $extra)=@_;
@@ -110,45 +65,26 @@ sub buildit {
     my $ram = $extra ? $extra : -1;
     my $c = "../tools/configure --type=n --target=$confnum --ram=$ram";
 
+    print "\nCompiling $target...\n" if($verbose);
     print "C: $c\n" if($verbose);
     `$c`;
 
-    print "Run 'make'\n" if($verbose);
-    `make VERSION=$version`;
+    print "Runing 'make'\n" if($verbose);
+    print "Using \'-j\' for make to enable mutil-core support" if($verbose && $mutilcore);
+    `make -j VERSION=$version` if($mutilcore);
+    `make    VERSION=$version` if(!$mutilcore);
 
     print "Run 'make zip'\n" if($verbose);
     `make zip`;
-
-    print "Run 'make mapzip'\n" if($verbose);
-    `make mapzip`;
-
-    print "Run 'make elfzip'\n" if($verbose);
-    `make elfzip`;
-}
-
-sub buildfonts {
-    my ($target, $confnum, $newl)=@_;
-
-    `rm -rf * >/dev/null 2>&1`;
-
-    my $ram = $extra ? $extra : -1;
-    my $c = "../tools/configure --type=n --target=$confnum --ram=$ram";
-
-    print "C: $c\n" if($verbose);
-    `$c`;
-
-    print "Run 'make fontzip'\n" if($verbose);
-    `make fontzip`;
 }
 
 # run make in tools first to make sure they're up-to-date
 print "cd tools && make\n" if($verbose);
-`(cd tools && make ) >/dev/null`;
+`(cd tools && make   ) >/dev/null` if($mutilcore);
+`(cd tools && make -j) >/dev/null` if(!$mutilcore);
 
-for my $b (&stablebuilds) {
+for my $b (&usablebuilds) {
     my $configname = $builds{$b}{configname} ? $builds{$b}{configname} : $b;
     runone($b, $configname, $builds{$b}{ram});
 }
-
-fonts("fonts", "iaudiox5");
 
